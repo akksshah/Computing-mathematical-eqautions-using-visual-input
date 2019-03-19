920ea058d7f236adfc9805223d371edfd1c929b9
Dhruv Vora <32409412+dhruv-vora@users.noreply.github.com>
2019-03-19 23:44:49 +0530

    Update linear_eq.py

--- linear_eq.py
+++ linear_eq.py
@@ -9,7 +9,7 @@ import digit_recognizer as dr
 
 def lineq():
     number = []
-    counter=0
+    counter = 0
     def linearequ():
 
         x = "Value of x = "
@@ -143,7 +143,7 @@ def lineq():
                 counter = 0
             else:
                 number.append(prediction2)
-        elif k == ord('l'):
+        elif k == ord('e'):
             x1 = number[0]
             y1 = number[1]
             c1 = number[2]
@@ -153,14 +153,9 @@ def lineq():
 
             print(x1)
             print(y1)
-
             print(c1)
-
             print(x2)
-
             print(y2)
-
-
             print(c2)
 
             # x1 = 3
@@ -170,7 +165,7 @@ def lineq():
             # y2 = 3
             # c2 = 7
 
-            #linearequ()
+            linearequ()
 
     cap.release()
     cv2.destroyAllWindows()
